add041ace08716190b3cecbdfba8e97ebde0b644
ForGetASD <vitayz60@gmail.com>
2022-10-11 13:22:28 +0200

    /tábla hozzáadása

---
+++ webAruhaz_Doga/database/migrations/2022_10_11_104631_create_product_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use app\Models\Product_type;
+
+class CreateProductTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {//Vitay Zalán
+        Schema::create('product_types', function (Blueprint $table) {
+            $table->bigIncrements('type_id');
+            $table->string('name', 40);
+            $table->longText('description', 150);
+            $table->integer('cost');
+            $table->timestamps();
+
+            $table->foreignId('type_id')->references('type_id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_types');
+    }
+}
